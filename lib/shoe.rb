e299287c3e3a4865090b7268ce943df00ccb018b
Christie Yoon <cleex1999@gmail.com>
2020-08-08 00:22:00 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,18 @@
+#learn spec/02_shoe_spec.rb
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+      if !(BRANDS.include?(brand))
+        BRANDS << brand
+      end
   end
+
 
   def cobble
     self.condition = "new"
